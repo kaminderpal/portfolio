b0ab040657363394918e83d96bdfa6bbb81d8545
kaminderpal <kaminderpal@livebarn.com>
2018-12-08 17:14:57 -0500

    implement about page

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import popperJs from 'popper.js';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './css/style.css'
-import '@fortawesome/fontawesome-free';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 if(module.hot){
      console.log("hot started vvvv vvv")
